046bdf51dcb21e3c183a3013ab62bd61292c4113
ikoncoders <ikoncoders@gmail.com>
2022-12-09 12:56:16 +0300

    add query builder interface and query builder factory

---
+++ src/MagmaCoreMvc/LiquidOrm/QueryBuilder/QueryBuilderFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ikonc\MagmaCoreMvc\LiquidOrm\QueryBuilder;
+
+use Ikonc\MagmaCoreMvc\Base\Exception\BaseUnexpectedValueException;
+
+
+class QueryBuilderFactory
+{
+
+    /**
+     * Main constructor method
+     * 
+     * @return void
+     */
+    public function __construct()
+    { }
+
+    public function __create(string $queryBuilderString) : QueryBuilderInterface
+    {
+        $queryBuilderObject = new $queryBuilderString();
+        if (!$queryBuilderString instanceof QueryBuilderInterface) {
+            throw new BaseUnexpectedValueException($queryBuilderString . ' is not a valid Query builder object.');
+        }
+        return $queryBuilderObject;
+    }
+    /**
+     * Create the QueryBuilder object
+     *
+     * @param string $queryBuilderString
+     * @return QueryBuilderInterface
+     */
+    public function create(string $queryBuilderString) : QueryBuilderInterface
+    {
+        $queryBuilderObject = new $queryBuilderString();
+        if (!$queryBuilderObject instanceof QueryBuilderInterface) {
+            throw new BaseUnexpectedValueException($queryBuilderString . ' is not a valid Query builder object.');
+        }
+        return $queryBuilderObject;
+    }
+
+
+}
